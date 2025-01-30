2d306b4f52120b36107abdf171a4a7a9aa5d3a1c
Chayan Nath <53663778+ChayanNath@users.noreply.github.com>
2025-01-31 00:53:59 +0530

    Updated buttons

--- apps/web/components/Canvas.tsx
+++ apps/web/components/Canvas.tsx
@@ -2,6 +2,7 @@
 
 import { initDraw } from "@/app/draw";
 import { Button } from "@workspace/ui/components/button";
+import { Circle, Minus, Square } from "lucide-react";
 import { useEffect, useRef } from "react";
 
 export default function Canvas({ slug }: { slug: string }) {
@@ -28,9 +29,16 @@ export default function Canvas({ slug }: { slug: string }) {
         ref={canvasRef}
         className="fixed top-0 left-0 w-screen h-screen"
       ></canvas>
-      <div className="fixed top-0 left-0 flex items-center justify-center gap-2">
-        <Button>Rectangle</Button>
-        <Button>Circle</Button>
+      <div className="fixed top-50 left-50 flex items-center justify-center gap-2 p-2">
+        <Button size="icon" title="Circle">
+          <Circle />
+        </Button>
+        <Button size="icon" title="Rectangle">
+          <Square />
+        </Button>
+        <Button size="icon" title="Line">
+          <Minus />
+        </Button>
       </div>
     </div>
   );
