c55962a3f59ac8486b55b84fe384886e69c2765c
Chayan Nath <53663778+ChayanNath@users.noreply.github.com>
2025-02-07 00:05:12 +0530

    build issue fix

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   transpilePackages: ["@workspace/ui"],
-}
+  env: {
+    NEXT_PUBLIC_BACKEND_URL:
+      process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000/api/v1",
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:8000",
+  },
+};
 
-export default nextConfig
+export default nextConfig;
